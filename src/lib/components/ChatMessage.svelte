c0e7a44a7c852942f5d4ba640058a7fade199e52
Eliécer Hernández Falcón <paraguanads@gmail.com>
2023-09-21 19:22:35 -0500

    nombre en chat

--- src/lib/components/ChatMessage.svelte
+++ src/lib/components/ChatMessage.svelte
@@ -8,13 +8,13 @@
 	<div class="chat-image avatar">
 		<div class="w-10 rounded-full">
 			<img
-				src="https://ui-avatars.com/api/?name={type === 'user' ? 'Yo' : 'B'}"
+				src="https://ui-avatars.com/api/?name={type === 'user' ? 'Yo' : 'C'}"
 				alt="{type} avatar"
 			/>
 		</div>
 	</div>
 	<div class="chat-header">
-		{type === 'user' ? 'Yo' : 'Bot'}
+		{type === 'user' ? 'Yo' : 'Compadre'}
 	</div>
 	<div class="chat-bubble {type === 'user' ? 'chat-bubble-primary' : 'chat-bubble-secondary'}">
 		{message}
